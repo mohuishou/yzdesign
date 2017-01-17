643c4664e4ab018695c0d39183093d432c847b78
mohuishou <1@lailin.xyz>
2017-01-18 00:40:21 +0800

    update

---
+++ src/applications/bbs/controller/AllUserController.php
@@ -0,0 +1,105 @@
+<?php
+defined('WEKIT_VERSION') || exit('Forbidden');
+
+Wind::import('SRV:user.vo.PwUserSo');
+Wind::import('SRV:forum.srv.threadList.PwSpaceThread');
+/**
+ * 会员列表
+ * Class AllUserController
+ * @author mohuishou <1@lailin.xyz>
+ */
+
+class AllUserController extends PwBaseController {
+    private $upgradeGroups = array('name' => '普通组', 'gid' => '0');
+
+    private $pageNumber = 10;
+	public function run() {
+
+        list( $sGroup, $page) = $this->getInput(array('gid', 'page'));
+        $vo = new PwUserSo();
+        !$sGroup || $vo->setGid($sGroup);
+        $page = intval($page) == 0 ? 1 : abs(intval($page));
+        /* @var $searchDs PwUserSearch */
+        $searchDs = Wekit::load('SRV:user.PwUserSearch');
+        $count = $searchDs->countSearchUser($vo);
+
+        $result = array();
+        if ($count > 0) {
+            $totalPage = ceil($count/$this->pageNumber);
+            $page > $totalPage && $page = $totalPage;
+            /* @var $searchDs PwUserSearch */
+            $searchDs = Wekit::load('user.PwUserSearch');
+            list($start, $limit) = Pw::page2limit($page, $this->pageNumber);
+            $result = $searchDs->searchUser($vo, $limit, $start);
+            if ($result) {
+                /* @var $userDs PwUser */
+                $userDs = Wekit::load('user.PwUser');
+                $list = $userDs->fetchUserByUid(array_keys($result), PwUser::FETCH_DATA);
+                $result = WindUtility::mergeArray($result, $list);
+            }
+        }
+
+        $users=$result;
+        foreach ($users as $k => $user){
+            $dataSource = new PwSpaceThread($user['uid']);
+            $threads=$dataSource->getData(3,0);
+
+//            地理位置获取
+            $users[$k]['address']="中国";
+            if($user['location_text']){
+                $tmp=explode(" ",$user['location_text']);
+                $users[$k]['address']=$tmp[0];
+            }
+
+//            最新发布的帖子
+            $user_threads=[];
+            foreach ($threads as $key => $thread){
+                $tmp['tid']=$thread['tid'];
+                $tmp['fid']=$thread['fid'];
+                $tmp['name']=$thread['subject'];
+                $tmp['thumb']=$this->getThumb($tmp['tid']);
+                $user_threads[]=$tmp;
+            }
+            $users[$k]['threads']=$user_threads;
+        }
+
+        //获取所有特殊用户组
+        $groupDs = Wekit::load('usergroup.PwUserGroups');
+        $groups = $groupDs->getGroupsByType("special");
+
+        $data = $vo->getData();
+        $this->setOutput($data, 'args');
+        $this->setOutput($page, 'page');
+        $this->setOutput($this->pageNumber, 'perpage');
+        $this->setOutput($count, 'count');
+        $this->setOutput($users, 'users');
+        $this->setOutput($groups, 'groups');
+		$this->setTemplate("all_user");
+
+
+	}
+
+    /**
+     * 获取缩略图路径
+     * @author mohuishou<1@lailin.xyz>
+     * @param $tid
+     * @return bool|string
+     */
+    private function getThumb($tid){
+        $pwThread=Wekit::load('attach.PwThreadAttach');
+        $data=$pwThread->fetchAttachByTid([$tid]);
+        foreach ($data as $v){
+            if($v['ifthumb']){
+                if($v['ifthumb']==2){
+                    return Pw::getPath($v['path'],2);
+                }else{
+                    return Pw::getPath($v['path'],1);
+                }
+
+            }
+        }
+
+        return false;
+    }
+
+}
--- src/applications/bbs/controller/IndexController.php
+++ src/applications/bbs/controller/IndexController.php
@@ -10,63 +10,76 @@ Wind::import('SRV:forum.srv.PwThreadList');
  * @package forum
  */
 
-class IndexController extends PwBaseController {
+class IndexController extends PwBaseController
+{
 
-	public function run() {
+    public $todayposts = 0;
+    public $article = 0;
 
-		$order = $this->getInput('order', 'get');
-		$page = intval($this->getInput('page', 'get'));
-		
-		$threadList = new PwThreadList();
-		$this->runHook('c_index_run', $threadList);
+    /* (non-PHPdoc)
+     * @see WindController::run()
+     */
+    public function run()
+    {
+        /* @var $forumDs PwForum */
+        $forumDs = Wekit::load('forum.PwForum');
+        $list = $forumDs->getCommonForumList(PwForum::FETCH_MAIN | PwForum::FETCH_STATISTICS);
 
-		$threadList->setPage($page)->setPerpage(Wekit::C('bbs', 'thread.perpage'));
-		
-		Wind::import('SRV:forum.srv.threadList.PwNewThread');
-		$forbidFids = Wekit::load('forum.srv.PwForumService')->getForbidVisitForum($this->loginUser, null, true);
-		$dataSource = new PwNewThread($forbidFids);
-		if ($order == 'postdate') {
-			$dataSource->setOrderBy($order);
-		} else {
-			$dataSource->setOrderBy('lastpost');
-		}
-		$threadList->execute($dataSource);
-		if ($threadList->total > 12000) {
-			Wekit::load('forum.PwThreadIndex')->deleteOver($threadList->total - 10000);
-		}
-		$threaddb = $threadList->getList();
-		$fids = array();
-		foreach ($threaddb as $key => $value) {
-			$fids[] = $value['fid'];
-		}
-		$forums = Wekit::load('forum.srv.PwForumService')->fetchForum($fids);
-		
-		if ($operateThread = $this->loginUser->getPermission('operate_thread', false, array())) {
-			$operateThread = Pw::subArray($operateThread, array('delete'));
-		}
-		
-		$this->setOutput($threadList, 'threadList');
-		$this->setOutput($threaddb, 'threaddb');
-		$this->setOutput($forums, 'forums');
-		$this->setOutput($threadList->icon, 'icon');
-		$this->setOutput($threadList->uploadIcon, 'uploadIcon');
-		$this->setOutput(26, 'numofthreadtitle');
-		$this->setOutput($order, 'order');
-		$this->setOutput($operateThread, 'operateThread');
-		
-		$this->setOutput($threadList->page, 'page');
-		$this->setOutput($threadList->perpage, 'perpage');
-		$this->setOutput($threadList->total, 'count');
-		$this->setOutput($threadList->maxPage, 'totalpage');
-		$this->setOutput($threadList->getUrlArgs(), 'urlargs');
-		
-		// seo设置
-		Wind::import('SRV:seo.bo.PwSeoBo');
-		$seoBo = PwSeoBo::getInstance();
-		$lang = Wind::getComponent('i18n');
-		$threadList->page <=1 && $seoBo->setDefaultSeo($lang->getMessage('SEO:bbs.forum.run.title'), '', $lang->getMessage('SEO:bbs.forum.run.description'));
-		$seoBo->init('bbs', 'new');
-		$seoBo->set('{page}', $threadList->page);
-		Wekit::setV('seo', $seoBo);
-	}
+        list($cateList, $forumList) = $this->_filterMap($list);
+        $bbsinfo = Wekit::load('site.PwBbsinfo')->getInfo(1);
+
+        $this->setOutput($cateList, 'cateList');
+        $this->setOutput($forumList, 'forumList');
+        $this->setOutput($this->todayposts, 'todayposts');
+        $this->setOutput($this->article, 'article');
+        $this->setOutput($bbsinfo, 'bbsinfo');
+        $this->setTemplate('index_run');
+
+        //seo设置
+        Wind::import('SRV:seo.bo.PwSeoBo');
+        $seoBo = PwSeoBo::getInstance();
+        $seoBo->init('bbs', 'forumlist');
+        Wekit::setV('seo', $seoBo);
+    }
+
+    /**
+     * 过滤版块信息
+     * 1、过滤掉不显示的版块
+     *
+     * @param array $list
+     * @return array
+     */
+    private function _filterMap($list)
+    {
+        $cate = $forum = array();
+        foreach ($list as $_key => $_item) {
+            if (1 != $_item['isshow']) continue;
+            $_item['manager'] = $this->_setManages(array_unique(explode(',', $_item['manager'])));
+            if ($_item['parentid'] == 0) {
+                $cate[$_key] = $_item;
+                isset($forum[$_key]) || $forum[$_key] = array();
+                $this->todayposts += $_item['todayposts'];
+                $this->article += $_item['article'];
+            } else {
+                $forum[$_item['parentid']][$_key] = $_item;
+            }
+        }
+        return array($cate, $forum);
+    }
+
+    /**
+     * 设置版块的版主UID
+     *
+     * @param array $manage
+     * @param array $userList
+     * @return array
+     */
+    private function _setManages($manage)
+    {
+        $_manage = array();
+        foreach ($manage as $_v) {
+            if ($_v) $_manage[] = $_v;
+        }
+        return $_manage;
+    }
 }
