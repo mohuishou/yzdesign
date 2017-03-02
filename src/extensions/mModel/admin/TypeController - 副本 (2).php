7e6c77e3329b1ce61376932108d6e515bf56b132
mohuishou <1@lailin.xyz>
2017-03-02 21:42:54 +0800

    update

--- src/extensions/mModel/admin/TypeController - 副本 (2).php
+++ src/extensions/mModel/admin/TypeController - 副本 (2).php
@@ -2,35 +2,28 @@
 defined('WEKIT_VERSION') or exit(403);
 Wind::import('ADMIN:library.AdminBaseController');
 
-/**
- * Class TypeController
- * 设置类型
- * @author mohuishou <1@lailin.xyz>
- */
-class TypeController extends AdminBaseController {
+class ManageController extends AdminBaseController {
 	
 	public function beforeAction($handlerAdapter) {
 		parent::beforeAction($handlerAdapter);
+		//TODO do something before all the action
 	}
 	
 	public function run() {
+		//TODO Insert your code here!
 	}
 
 	public function addAction(){
 
 	}
 
+	public function deleteAction(){
+
+	}
+
 	public function editAction(){
 		
 	}
-
-    public function deleteAction(){
-
-    }
-
-    private function typeDao(){
-
-    }
 }
 
 ?>
